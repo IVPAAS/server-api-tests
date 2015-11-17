aba3aa10f601742726ad0ed00b09068870306503
root <root@localhost.localdomain>
2015-11-17 16:49:46 -0500

    refactor

--- apiTests/InVideoQuizTests.php
+++ apiTests/InVideoQuizTests.php
@@ -1,191 +1,10 @@
 <?php
 require_once('/opt/kaltura/web/content/clientlibs/php5/KalturaClient.php');
-function printUsage()
-{
-    print ("\n\rUsage: " .$GLOBALS['argv'][0] . " <DC URL> 	<parnter id> <admin secret>");
-    print ("\n\r for adding quiz.\r\n");
-}
-//start session and setting KS function
-class bcolors
-{
-    const OKBLUE = "\033[94m";
-    const OKGREEN = "\033[92m";
-    const WARNING = "\033[93m";
-    const FAIL = "\033[91m";
-    const ENDC = "\033[0m";
-    const BOLD = "\033[1m";
-    const UNDERLINE = "\033[4m";
-}
-function success($msg)
-{
-    print("\n".bcolors::OKGREEN.$msg." OK!".bcolors::ENDC);
-    return 0;
-}
-function fail($msg)
-{
-    print("\n".bcolors::FAIL.$msg." FAIL!".bcolors::ENDC);
-    return -1;
-}
+require_once('apiTestHelper.php');
+require_once('InVideoQuizHelper.php');
 
-function startKalturaSession($partnerId,$secret,$destUrl,$type=KalturaSessionType::ADMIN,$userId=null) 
-{
-	try
-	{
-		$config = new KalturaConfiguration($partnerId);
-		$config->serviceUrl = $destUrl;
-        	$client = new KalturaClient($config);
-		$result = $client->session->start($secret, $userId, $type, $partnerId, null, null);
-		$client->setKs($result);
-		//print("Started session successfully with KS [$result]\n");
-		return $client;
-	}
-	catch (KalturaException $e)
-	{
-		$msg = $e->getMessage();
-		shout("Problem starting session with message: [$msg]\n");
-		die("ERROR - cannot generate session with partner id [$partnerId] and secret [$secret]");
-	}
-}	
 
-function startWidgetSession($destUrl,$partnerId)
-{
-    try
-    {
-        $config = new KalturaConfiguration($partnerId);
-        $config->serviceUrl = $destUrl;
-        $client = new KalturaClient($config);
-        $widgetId = "_".$partnerId;
-        $expiry = null;
-        $result = $client->session->startwidgetsession($widgetId, $expiry);  
-        $client->setKs($result->ks);
-        return $client;
-    }
-	catch (KalturaException $e)
-	{
-		$msg = $e->getMessage();
-		shout("Problem starting widget sesseion with message: [$msg]\n");
-		die("ERROR - cannot generate widget session with widgetId [$widgetId]");
-	}
-}
 
-function createNewQuiz($client,
-                       $entryId,
-                       $showResultOnAnswer,
-                       $showCorrectKeyOnAnswer,
-                       $allowAnswerUpdate,
-                       $showCorrectAfterSubmission,
-                       $allowDownload,
-                       $showGradeAfterSubmission)
-{
-  $quiz                               = new KalturaQuiz();
-  $quiz->showResultOnAnswer           = $showResultOnAnswer;
-  $quiz->showCorrectKeyOnAnswer       = $showCorrectKeyOnAnswer;
-  $quiz->allowAnswerUpdate            = $allowAnswerUpdate;
-  $quiz->showCorrectAfterSubmission   = $showCorrectAfterSubmission;
-  $quiz->allowDownload                = $allowDownload;
-  $quiz->showGradeAfterSubmission     = $showGradeAfterSubmission;
-  $quizPlugin                         = KalturaQuizClientPlugin::get($client);
-  $result                             = $quizPlugin->quiz->add($entryId, $quiz);
-  return $result;
-}
-
-function addKalturaUser($client,$userId)
-{
-  
-  $filter          = new KalturaUserFilter();
-  $filter->idEqual = $userId;
-  $pager           = null;
-  $result          = $client->user->listAction($filter, $pager);
-  
-  if($result->totalCount==0)
-  {
-    $user             = new KalturaUser();
-    $user->id         = $userId;
-    $user->type       = KalturaUserType::USER;
-    $user->screenName = $userId;
-    $user->fullName   = $userId;
-    $user->email      = $userId."@m.com";
-    $result           = $client->user->add($user);
-  }
-  else
-  {
-    $result           = $result->objects[0];
-  }
-  
-  //print ("\nAdd User ID:".$result->id);
-  return $result;
-}
-
-function addQuizUserEntry($client,$userId,$quizEntryId)
-{
-  $userEntry           = new KalturaQuizUserEntry();
-  $userEntry->entryId  = $quizEntryId;
-  $userEntry->userId   = $userId;
-  $result              = $client->userEntry->add($userEntry);
-  //print ("\nAdd UserEntry ID:".$result->id);
-  return $result;
-}
-
-function addQuestionsOnQuiz($client,$QuizEntryId,$str)
-{
-  $cuePoint                               = new KalturaQuestionCuePoint();
-  $cuepointPlugin                         = KalturaCuepointClientPlugin::get($client);
-  $cuePoint->hint                         = 'h'+$str;
-  $cuePoint->question                     = 'q'+$str;
-  $cuePoint->explanation                  = 'r'+$str;
-  $cuePoint->entryId                      = $QuizEntryId;  
-  $cuePoint->optionalAnswers              = array();
-  $cuePoint->optionalAnswers[0]           = new KalturaOptionalAnswer();
-  $cuePoint->optionalAnswers[0]->key      = $str;
-  $cuePoint->optionalAnswers[0]->isCorrect= 1;
-  $cuePoint->optionalAnswers[1]           = new KalturaOptionalAnswer();
-  $cuePoint->optionalAnswers[1]->key      = $str;
-  $cuePoint->optionalAnswers[1]->isCorrect= 1;
-  $cuePoint->hint                         = 'Hint';
-  $cuePoint->question                     = 'What is my name?';
-  $cuePoint->explanation                  = 'My Name';
-  $cuepointPlugin                         = KalturaCuepointClientPlugin::get($client);
-  try 
-  {
-    $result                               = $cuepointPlugin->cuePoint->add($cuePoint);
-  }
-  catch (Exception $e)
-  {
-    //print_r($e);
-  }
-  //print ("\nQuestion ID:".$result->id);
-
-  return $result; 
-}
-
-function addAnswer($client,$quizEntryId,$questionId,$quizUserEntry,$answerTxt)
-{
-  $cuePoint                               = new KalturaAnswerCuePoint();
-  $cuePoint->entryId                      = $quizEntryId;
-  $cuePoint->parentId                     = $questionId;
-  $cuePoint->quizUserEntryId              = $quizUserEntry;
-  $cuepointPlugin                         = KalturaCuepointClientPlugin::get($client);
-  $cuePoint->answerKey                    = $answerTxt; 
-  $result                                 = $cuepointPlugin->cuePoint->add($cuePoint);
-  //print (" , ".$result->id);
-  return ($result);
-}
-function addEntry($client,$name)
-{
-  $entry                                  = new KalturaBaseEntry();
-  $type                                   = KalturaEntryType::MEDIA_CLIP;
-  $entry->name                            = $name;
-  $result                                 = $client->baseEntry->add($entry, $type);
-  print ("\nAdd entry ID:".$result->id);
-  return $result;
-}
-
-function submitQuiz($client,$userEntryId)
-{
-  $result                                 = $client->userEntry->submitquiz($userEntryId);
-  //print("\nSubmit Quiz:".$userEntryId);
-  return $result;
-}
 
 function report1($client,$userEntryId)
 {
@@ -342,9 +161,9 @@ function Test5_CheckAllowDownload($client)
   {
       fail(__FUNCTION__." Should get download URL ".$res->score);
   }
-  
-  $quiz = new KalturaQuiz();
-  $quiz->allowDownload = KalturaNullableBoolean::FALSE_VALUE;
+  //print_r($result);
+  $entry=addEntry($client,__FUNCTION__);
+  $quiz = createNewQuiz($client,$entry->id,null,null,null,null,KalturaNullableBoolean::FALSE_VALUE,null);
   $quizPlugin = KalturaQuizClientPlugin::get($client);
   $result = $quizPlugin->quiz->update($entry->id, $quiz);
   try
@@ -459,8 +278,6 @@ function test7_GetUserPercentageReport($client)
     {
         fail(__FUNCTION__." score is no calculated correct, should be 50 got - ".$scores[1]);
     }
-
-
     $user = addKalturaUser($client,"UU".rand(1,1000));
     $quizUserEntry = addQuizUserEntry($client,$user->id,$entry->id);
     for ( $answerIndex=0 ; $answerIndex < 4 ; $answerIndex ++)
@@ -480,37 +297,21 @@ function test7_GetUserPercentageReport($client)
 }
 
 
-function mainStory($dc,$partnerId,$adminSecret,$userSecret)
+function main($dc,$partnerId,$adminSecret,$userSecret)
 {
   $client = startKalturaSession($partnerId,$adminSecret,$dc); 
-  Test1_Basicflow($client);
-  Test2_ValidateNoScoreUponSubmit($client,$partnerId,$userSecret,$dc);
-  Test3_ValidateScoreUponSubmitWithAdminKS($client);
-  Test4_ValidateScoreUponSubmit($client,$partnerId,$userSecret,$dc);
-  Test5_CheckAllowDownload($client);
-  Test5_1_CheckAllowDownloadWithWidgetKs($client,$dc,$partnerId);
-  Test6_ValidateshowCorrectAfterSubmission($client);
-  test7_GetUserPercentageReport($client);
-  // report1($client,$quizUserEntry->id);
-  print ("\n");
-  
+  $ret=Test1_Basicflow($client);
+  $ret+=Test2_ValidateNoScoreUponSubmit($client,$partnerId,$userSecret,$dc);
+  $ret+=Test3_ValidateScoreUponSubmitWithAdminKS($client);
+  $ret+=Test4_ValidateScoreUponSubmit($client,$partnerId,$userSecret,$dc);
+  $ret+=Test5_CheckAllowDownload($client);
+  $ret+=Test5_1_CheckAllowDownloadWithWidgetKs($client,$dc,$partnerId);
+  $ret+=Test6_ValidateshowCorrectAfterSubmission($client);
+  $ret+=test7_GetUserPercentageReport($client);
+  return ($ret);
 }
 
-
-
-if ($argc!=5 )
-{
-    //printUsage();
-	exit (1);
-}
-
-$dcUrl 			= 	$argv[1];
-$partnerId 		= 	$argv[2];
-$adminSecret	= 	$argv[3];
-$userSecret	    = 	$argv[4];
-mainStory($dcUrl,$partnerId,$adminSecret,$userSecret);
-
-exit(0);
+goMain();
 
 
 
