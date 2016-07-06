7b977e100e614435d25c1e5e4d26cb42ff2419a1
MosheMaorKaltura <moshe.maor@kaltura.com>
2016-07-06 14:34:36 -0200

    add test for update exting entries

--- apiTests/basicTests/entryWithParentTest.php
+++ apiTests/basicTests/entryWithParentTest.php
@@ -32,9 +32,17 @@ function test1CreateEntryWithParent($client)
 	$pager = null;
 	$result = $client->baseEntry->listAction($filter, $pager);
 
+	$foundParent=false;
+
 	foreach($result->objects as $entry)
+	{
 		if($entry->id == $childEntry->id)
 			return (fail(__FUNCTION__));
+		if($entry->id == $parentEntry->id)
+			$foundParent=true;
+	}
+	if(!$foundParent)
+		fail(__FUNCTION__."Should have found parent entry but it did not");
 
 	success(__FUNCTION__);
 	//search the entries
@@ -51,18 +59,54 @@ function test2CreateEntryWithParentAndTempalte($client)
 	$filter = new KalturaMediaEntryFilter();
         $filter->orderBy = '-createdAt';
         $filter->statusIn = '-1,-2,0,1,2,7,4';
-        $pager = null;
-        $result = $client->baseEntry->listAction($filter, $pager);
+        $result = $client->baseEntry->listAction($filter, null);
+
+	$foundParent=false;
+        foreach($result->objects as $entry)
+        {
+                if($entry->id == $childEntry->id)
+                        return (fail(__FUNCTION__));
+                if($entry->id == $parentEntry->id)
+                        $foundParent=true;
+        }
+        if(!$foundParent)
+                fail(__FUNCTION__."Should have found parent entry but it did not");
+
+        success(__FUNCTION__);
+}
+
+function test3ChangeExsitingEntry($client)
+{
+        $templateEntry = myAddEntry($client);
+        $parentEntry = myAddEntry($client);
+        $childEntry = myAddEntry($client,null,$templateEntry->id);
+
+        info("Created entries: parent - $parentEntry->id child(not yes connected)- $childEntry->id template $templateEntry->id\n");
+
+        $filter = new KalturaMediaEntryFilter();
+        $filter->orderBy = '-createdAt';
+        $filter->statusIn = '-1,-2,0,1,2,7,4';
+        $result = $client->baseEntry->listAction($filter, null);
+
+	$foundInSearch=false;
 
         foreach($result->objects as $entry)
                 if($entry->id == $childEntry->id)
-                        return (fail(__FUNCTION__));
+			$foundInSearch=true;
 
-        success(__FUNCTION__);
+	if($foundInSearch!=true)
+		return (fail(__FUNCTION__."Entry should have been found but it was not"));
 
+	$baseEntry = new KalturaBaseEntry();
+	$baseEntry->parentEntryId = $parentEntry->id;
+	$result = $client->baseEntry->update($childEntry->id, $baseEntry);
 
-
-	//search the entries
+	$result = $client->baseEntry->listAction($filter, null);
+	foreach($result->objects as $entry)
+                if($entry->id == $childEntry->id)
+                	return (fail(__FUNCTION__."Entry should not have been found but it was"));
+ 
+	return (success(__FUNCTION__));
 }
 
 
@@ -72,6 +116,7 @@ function main($dc,$partnerId,$adminSecret,$userSecret)
 	//todo - create entry
 	test1CreateEntryWithParent($client);
 	test2CreateEntryWithParentAndTempalte($client);
+	test3ChangeExsitingEntry($client);
 	return ;
 }
 
