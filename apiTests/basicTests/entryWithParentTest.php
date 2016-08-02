c2e6f2ae6b83bb68361cb6f805c3efcdca59cecc
gotlieb <gotlieb.erez@gmail.com>
2016-08-02 16:44:12 +0300

    added New tests for annotationCuePoints and copying collabrators from template entry and modified inVideoQuiz to print more info in tests.

--- apiTests/basicTests/entryWithParentTest.php
+++ apiTests/basicTests/entryWithParentTest.php
@@ -21,6 +21,7 @@ function myAddEntry($client,$parentEntryId=null,$templateEntryId=null)
 
 function test1CreateEntryWithParent($client)
 {
+	info('start ' .  __FUNCTION__);
 	$parentEntry = myAddEntry($client);
 	$childEntry = myAddEntry($client,$parentEntry->id);
 
@@ -50,74 +51,99 @@ function test1CreateEntryWithParent($client)
 
 function test2CreateEntryWithParentAndTempalte($client)
 {
+	info('start ' . __FUNCTION__);
 	$templateEntry = myAddEntry($client);
 	$parentEntry = myAddEntry($client);
-	$childEntry = myAddEntry($client,$parentEntry->id,$templateEntry->id);
+	$childEntry = myAddEntry($client, $parentEntry->id, $templateEntry->id);
 
 	info("Created entries: parent - $$parentEntry->id child - $childEntry->id template $templateEntry->id\n");
 
 	$filter = new KalturaMediaEntryFilter();
-        $filter->orderBy = '-createdAt';
-        $filter->statusIn = '-1,-2,0,1,2,7,4';
-        $result = $client->baseEntry->listAction($filter, null);
+	$filter->orderBy = '-createdAt';
+	$filter->statusIn = '-1,-2,0,1,2,7,4';
+	$result = $client->baseEntry->listAction($filter, null);
 
-	$foundParent=false;
-        foreach($result->objects as $entry)
-        {
-                if($entry->id == $childEntry->id)
-                        return (fail(__FUNCTION__));
-                if($entry->id == $parentEntry->id)
-                        $foundParent=true;
-        }
-        if(!$foundParent)
-                fail(__FUNCTION__."Should have found parent entry but it did not");
+	$foundParent = false;
+	foreach ($result->objects as $entry)
+	{
+		if ($entry->id == $childEntry->id)
+			return (fail(__FUNCTION__));
+		if ($entry->id == $parentEntry->id)
+			$foundParent = true;
+	}
+	if (!$foundParent)
+		fail(__FUNCTION__ . "Should have found parent entry but it did not");
 
-        success(__FUNCTION__);
+	success(__FUNCTION__);
 }
 
 function test3ChangeExsitingEntry($client)
 {
-        $templateEntry = myAddEntry($client);
-        $parentEntry = myAddEntry($client);
-        $childEntry = myAddEntry($client,null,$templateEntry->id);
+	info('start ' . __FUNCTION__);
 
-        info("Created entries: parent - $parentEntry->id child(not yes connected)- $childEntry->id template $templateEntry->id\n");
+	$templateEntry = myAddEntry($client);
+	$parentEntry = myAddEntry($client);
+	$childEntry = myAddEntry($client, null, $templateEntry->id);
 
-        $filter = new KalturaMediaEntryFilter();
-        $filter->orderBy = '-createdAt';
-        $filter->statusIn = '-1,-2,0,1,2,7,4';
-        $result = $client->baseEntry->listAction($filter, null);
+	info("Created entries: parent - $parentEntry->id child(not yes connected)- $childEntry->id template $templateEntry->id\n");
 
-	$foundInSearch=false;
+	$filter = new KalturaMediaEntryFilter();
+	$filter->orderBy = '-createdAt';
+	$filter->statusIn = '-1,-2,0,1,2,7,4';
+	$result = $client->baseEntry->listAction($filter, null);
 
-        foreach($result->objects as $entry)
-                if($entry->id == $childEntry->id)
-			$foundInSearch=true;
+	$foundInSearch = false;
 
-	if($foundInSearch!=true)
-		return (fail(__FUNCTION__."Entry should have been found but it was not"));
+	foreach ($result->objects as $entry)
+		if ($entry->id == $childEntry->id)
+			$foundInSearch = true;
+
+	if ($foundInSearch != true)
+		return (fail(__FUNCTION__ . "Entry should have been found but it was not"));
 
 	$baseEntry = new KalturaBaseEntry();
 	$baseEntry->parentEntryId = $parentEntry->id;
 	$result = $client->baseEntry->update($childEntry->id, $baseEntry);
 
 	$result = $client->baseEntry->listAction($filter, null);
-	foreach($result->objects as $entry)
-                if($entry->id == $childEntry->id)
-                	return (fail(__FUNCTION__."Entry should not have been found but it was"));
- 
+	foreach ($result->objects as $entry)
+		if ($entry->id == $childEntry->id)
+			return (fail(__FUNCTION__ . "Entry should not have been found but it was"));
+
 	return (success(__FUNCTION__));
 }
 
+function test4CreateEntryWithTempalteAndCopyCollaborators($client)
+{
+	info('start ' . __FUNCTION__);
+
+	$templateEntry = myAddEntry($client);
+
+	$baseEntry = new KalturaBaseEntry();
+	$baseEntry->entitledUsersEdit = 'TestUserEdit';
+	$baseEntry->entitledUsersPublish = 'TestUserPublish';
+	$result = $client->baseEntry->update($templateEntry->id, $baseEntry);
+
+	$childEntry = myAddEntry($client, null, $templateEntry->id);
+
+	if ($childEntry->entitledUsersPublish != 'TestUserPublish')
+		return (fail(__FUNCTION__ . "entitledUsersPublish should have been copied from templateEntry [".$result->entitledUsersPublish . "] but actual was: " .$childEntry->entitledUsersPublish));
+
+	if ($childEntry->entitledUsersEdit != 'TestUserEdit')
+		return (fail(__FUNCTION__ . "entitledUsersPublish should have been copied from templateEntry [".$result->entitledUsersEdit . "] but actual was: " .$childEntry->entitledUsersEdit));
+
+	return (success(__FUNCTION__));
+}
 
 function main($dc,$partnerId,$adminSecret,$userSecret)
 {
 	$client = startKalturaSession($partnerId,$adminSecret,$dc);
 	//todo - create entry
-	test1CreateEntryWithParent($client);
-	test2CreateEntryWithParentAndTempalte($client);
-	test3ChangeExsitingEntry($client);
-	return ;
+	$ret = test1CreateEntryWithParent($client);
+	$ret += test2CreateEntryWithParentAndTempalte($client);
+	$ret += test3ChangeExsitingEntry($client);
+	$ret += test4CreateEntryWithTempalteAndCopyCollaborators($client);
+	return $ret;
 }
 
 goMain();
