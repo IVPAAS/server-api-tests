64fb0687f590d784788a8d19fc2e1997eed3190d
EladCohenKaltura <elad.cohen@kaltura.com>
2017-06-06 11:55:40 +0300

    PLAT-7508 test hack same poll vote PLAT-7508 test hack same poll vote

--- apiTests/basicTests/pollTest.php
+++ apiTests/basicTests/pollTest.php
@@ -7,6 +7,15 @@ function getPollId($client)
 	return $client->poll->add();
 }
 
+function comparePollVote($string, $arrayString)
+{
+	$explodedArray = explode(',', $string);
+	$diffArray = array_diff(json_decode($arrayString), $explodedArray);
+	if (count($diffArray) !==0 )
+		return false;
+	return true;
+}
+
 function validatePollResultStructure($votes, $expectedVoters, $expectedPollId, $expectedCounters, $funcName)
 {
 	if (!$votes)
@@ -64,7 +73,7 @@ function Test1_PollAdd($client)
 function Test2_FullPollTestSingularAnsSingleUserOneVote($client)
 {
 	$pollId = $client->poll->add();
-	$client->poll->vote(strval($pollId), "myUser1", "1");
+	$client->poll->vote($pollId, "myUser1", "1");
 	$votes = $client->poll->getVotes($pollId, "1");
 	return validatePollResultStructure($votes, 3, $pollId, array(1 => 1), __FUNCTION__);
 }
@@ -72,9 +81,9 @@ function Test2_FullPollTestSingularAnsSingleUserOneVote($client)
 function Test3_FullPollTestSingularAnsMultiUserOneVote($client)
 {
 	$pollId = $client->poll->add();
-	$client->poll->vote(strval($pollId), "myUser1", "1");
-	$client->poll->vote(strval($pollId), "myUser2", "1");
-	$client->poll->vote(strval($pollId), "myUser3", "1");
+	$client->poll->vote($pollId, "myUser1", "1");
+	$client->poll->vote($pollId, "myUser2", "1");
+	$client->poll->vote($pollId, "myUser3", "1");
 	$votes = $client->poll->getVotes($pollId, "1");
 	return validatePollResultStructure($votes, 1, $pollId, array(1 => 3), __FUNCTION__);
 }
@@ -82,12 +91,12 @@ function Test3_FullPollTestSingularAnsMultiUserOneVote($client)
 function Test4_FullPollTestSingularAnsMultiUserSeveralVotes($client)
 {
 	$pollId = $client->poll->add();
-	$client->poll->vote(strval($pollId), "myUser1", "1");
-	$client->poll->vote(strval($pollId), "myUser2", "2");
-	$client->poll->vote(strval($pollId), "myUser3", "3");
-	$client->poll->vote(strval($pollId), "myUser4", "2");
-	$client->poll->vote(strval($pollId), "myUser5", "1");
-	$client->poll->vote(strval($pollId), "myUser6", "1");
+	$client->poll->vote($pollId, "myUser1", "1");
+	$client->poll->vote($pollId, "myUser2", "2");
+	$client->poll->vote($pollId, "myUser3", "3");
+	$client->poll->vote($pollId, "myUser4", "2");
+	$client->poll->vote($pollId, "myUser5", "1");
+	$client->poll->vote($pollId, "myUser6", "1");
 	$votes = $client->poll->getVotes($pollId, "1,2,3");
 	return validatePollResultStructure($votes, 6, $pollId, array(1 => 3, 2=>2, 3=>1), __FUNCTION__);
 }
@@ -95,11 +104,11 @@ function Test4_FullPollTestSingularAnsMultiUserSeveralVotes($client)
 function Test5_FullPollTestSingularAnsMultiUserRevote($client)
 {
 	$pollId = $client->poll->add();
-	$client->poll->vote(strval($pollId), "myUser1", "1");
-	$client->poll->vote(strval($pollId), "myUser2", "2");
-	$client->poll->vote(strval($pollId), "myUser3", "3");
-	$client->poll->vote(strval($pollId), "myUser2", "1");
-	$client->poll->vote(strval($pollId), "myUser3", "1");
+	$client->poll->vote($pollId, "myUser1", "1");
+	$client->poll->vote($pollId, "myUser2", "2");
+	$client->poll->vote($pollId, "myUser3", "3");
+	$client->poll->vote($pollId, "myUser2", "1");
+	$client->poll->vote($pollId, "myUser3", "1");
 	$votes = $client->poll->getVotes($pollId, "1,2,3");
 	return validatePollResultStructure($votes, 3, $pollId, array(1 => 3, 2=>0, 3=>0), __FUNCTION__);
 }
@@ -107,11 +116,11 @@ function Test5_FullPollTestSingularAnsMultiUserRevote($client)
 function Test6_FullPollTestMultiAnsMultiUserRevote($client)
 {
 	$pollId = $client->poll->add();
-	$client->poll->vote(strval($pollId), "myUser1", "1,2");
-	$client->poll->vote(strval($pollId), "myUser2", "2,4");
-	$client->poll->vote(strval($pollId), "myUser3", "3,1");
-	$client->poll->vote(strval($pollId), "myUser2", "1,4");
-	$client->poll->vote(strval($pollId), "myUser3", "1");
+	$client->poll->vote($pollId, "myUser1", "1,2");
+	$client->poll->vote($pollId, "myUser2", "2,4");
+	$client->poll->vote($pollId, "myUser3", "3,1");
+	$client->poll->vote($pollId, "myUser2", "1,4");
+	$client->poll->vote($pollId, "myUser3", "1");
 	$votes = $client->poll->getVotes($pollId, "1,2,3,4");
 	return validatePollResultStructure($votes, 3, $pollId, array(1 => 3, 2=>1, 3=>0, 4=>1), __FUNCTION__);
 }
@@ -119,11 +128,11 @@ function Test6_FullPollTestMultiAnsMultiUserRevote($client)
 function Test7_FullPollTestMultiAnsMultiUserRevoteWithZeroes($client)
 {
 	$pollId = $client->poll->add();
-	$client->poll->vote(strval($pollId), "myUser1", "0,1,2");
-	$client->poll->vote(strval($pollId), "myUser2", "2,4");
-	$client->poll->vote(strval($pollId), "myUser3", "3,1,0");
-	$client->poll->vote(strval($pollId), "myUser2", "1,4");
-	$client->poll->vote(strval($pollId), "myUser3", "1,0");
+	$client->poll->vote($pollId, "myUser1", "0,1,2");
+	$client->poll->vote($pollId, "myUser2", "2,4");
+	$client->poll->vote($pollId, "myUser3", "3,1,0");
+	$client->poll->vote($pollId, "myUser2", "1,4");
+	$client->poll->vote($pollId, "myUser3", "1,0");
 	$votes = $client->poll->getVotes($pollId, "0,1,2,3,4");
 	return validatePollResultStructure($votes, 3, $pollId, array(0=> 2, 1 => 3, 2=>1, 3=>0, 4=>1), __FUNCTION__);
 }
@@ -134,18 +143,51 @@ function Test8_FullPollTestMediumScale($client)
 	$pollId = $client->poll->add();
 	for ($index = 0 ; $index < $max ; $index++)
 	{
-		$client->poll->vote(strval($pollId), "myUser".$index, "0,1,2");
+		$client->poll->vote($pollId, "myUser".$index, "0,1,2");
 	}
 
 	$revote=751;
 	for ($index = 0 ; $index < $revote ; $index++)
 	{
-		$client->poll->vote(strval($pollId), "myUser".$index, "1,3");
+		$client->poll->vote($pollId, "myUser".$index, "1,3");
 	}
 	$votes = $client->poll->getVotes($pollId, "0,1,2,3,4");
 	return validatePollResultStructure($votes, $max, $pollId, array( 0=>$max-$revote, 1=>$max, 2=>$max-$revote, 3=>$revote, 4=>0), __FUNCTION__);
-
 }
+
+function Test9_GetEarlierVoteTest($client)
+{
+	$pollId = $client->poll->add();
+	$client->poll->vote($pollId, "myUser1", "0,1,2");
+	$userVote = $client->poll->getVote($pollId, "myUser1");
+	if (!comparePollVote("0,1,2", $userVote))
+		return fail("Get vote gave different result: $userVote than expected : 0,1,2 ");
+	$client->poll->vote($pollId, "myUser1", "2,4");
+	$client->poll->vote($pollId, "myUser1", "3,1,0");
+	$userVote = $client->poll->getVote($pollId, "myUser1");
+	if (!comparePollVote("3,1,0", $userVote))
+		return fail("Get vote gave different result: $userVote than expected : 3,1,0 ");
+
+	$client->poll->vote($pollId, "myUser2", "1");
+	$userVote = $client->poll->getVote($pollId, "myUser2");
+	if (!comparePollVote("1", $userVote))
+		return fail("Get vote gave different result: $userVote than expected : 1 ");
+	$client->poll->vote($pollId, "myUser2", "0");
+	$userVote = $client->poll->getVote($pollId, "myUser2");
+	if (!comparePollVote("0", $userVote))
+		return fail("Get vote gave different result: $userVote than expected : 0 ");
+
+	return success(__FUNCTION__);
+}
+
+function Test10_VoteForSameAnswer($client)
+{
+	$pollId = $client->poll->add();
+	$client->poll->vote($pollId, "myUser1", "1,1,1,1,2,3,2,3,1,1,1,1,2,3");
+	$votes = $client->poll->getVotes($pollId, "1");
+	return validatePollResultStructure($votes, 1, $pollId, array(1 => 1, 2=>1, 3=>1), __FUNCTION__);
+}
+
 
 
 function main($dc,$partnerId,$adminSecret,$userSecret)
@@ -159,6 +201,8 @@ function main($dc,$partnerId,$adminSecret,$userSecret)
 	$ret += Test6_FullPollTestMultiAnsMultiUserRevote($client);
 	$ret += Test7_FullPollTestMultiAnsMultiUserRevoteWithZeroes($client);
 	$ret += Test8_FullPollTestMediumScale($client);
+	$ret += Test9_GetEarlierVoteTest($client);
+	$ret += Test10_VoteForSameAnswer($client);
 
 	return ($ret);
 }
